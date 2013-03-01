d30e0a2c68d9ad08b98f113d97ae7affd9c689e4
samiy-xx <sami.ylonen@gmail.com>
2013-03-01 20:08:44 +0100

    break stuff

--- web/datachannel_filetransfer_sample.dart
+++ web/datachannel_filetransfer_sample.dart
@@ -1,11 +1,71 @@
 import "dart:html";
+import "dart:async";
 import '../lib/demo_client.dart';
 
-import 'package:dart_rtc_common/rtc_common.dart';
-import 'package:dart_rtc_client/rtc_client.dart';
-
+//import 'package:dart_rtc_common/rtc_common.dart';
+//import 'package:dart_rtc_client/rtc_client.dart';
+import '../../dart_rtc_client/lib/rtc_client.dart';
+import '../../dart_rtc_common/lib/rtc_common.dart';
 
 void main() {
-  
+  int channelLimit = 5;
+  String otherId;
+  FileUploadInputElement fuie = query("#file");
+
+  String test = "abcdefghijklmnopqrstuvwxyz1234567890";
+  ArrayBuffer buffer = BinaryData.bufferFromString(test);
+  String test2 = BinaryData.stringFromBuffer(buffer);
+  print(test);
+  print(test2);
+
+  ChannelClient qClient = new ChannelClient(new WebSocketDataSource("ws://127.0.0.1:8234/ws"))
+  //.setChannel("abc")
+  .setRequireAudio(false)
+  .setRequireVideo(false)
+  .setRequireDataChannel(true)
+  .setAutoCreatePeer(true);
+
+  qClient.onInitializationStateChangeEvent.listen((InitializationStateEvent e) {
+
+    if (e.state == InitializationState.CHANNEL_READY) {
+      fuie.onChange.listen((Event e) {
+        File f = fuie.files[0];
+        FileReader reader = new FileReader();
+        reader.onLoad.listen((ProgressEvent e) {
+          qClient.sendArrayBuffer(otherId, reader.result);
+        });
+
+        reader.readAsArrayBuffer(f);
+      });
+    }
+
+    if (e.state == InitializationState.REMOTE_READY) {
+
+      qClient.joinChannel("abc");
+    }
+  });
+
+  qClient.onSignalingOpenEvent.listen((SignalingOpenEvent e) {
+
+    qClient.setChannelLimit(channelLimit);
+  });
+
+
+
+  qClient.onSignalingCloseEvent.listen((SignalingCloseEvent e) {
+    new Timer(const Duration(milliseconds: 10000), () {
+      qClient.initialize();
+    });
+  });
+
+  qClient.onPacketEvent.listen((PacketEvent e) {
+    print("packet ${e.type.toString()}");
+    if (e.type.toString() == PacketType.ID.toString() || e.type.toString() == PacketType.JOIN.toString()) {
+      print("assigning id ${e.packet.id}");
+      otherId = e.packet.id;
+    }
+  });
+
+  qClient.initialize();
 }
 
