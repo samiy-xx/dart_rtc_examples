634f2d881ad85d69c0cb956ee77c6f97370665d4
Sami Ylonen <sami.ylonen@gmail.com>
2013-03-04 22:46:36 +0100

    filetransfer madness

--- web/datachannel_filetransfer_sample.dart
+++ web/datachannel_filetransfer_sample.dart
@@ -11,13 +11,13 @@ void main() {
   int channelLimit = 5;
   String otherId;
   FileUploadInputElement fuie = query("#file");
-
+  DivElement output = query("#output");
   String test = "abcdefghijklmnopqrstuvwxyz1234567890";
   ArrayBuffer buffer = BinaryData.bufferFromString(test);
   String test2 = BinaryData.stringFromBuffer(buffer);
   print(test);
   print(test2);
-
+  new Logger().setLevel(LogLevel.ERROR);
   ChannelClient qClient = new ChannelClient(new WebSocketDataSource("ws://127.0.0.1:8234/ws"))
   //.setChannel("abc")
   .setRequireAudio(false)
@@ -63,6 +63,22 @@ void main() {
     if (e.type.toString() == PacketType.ID.toString() || e.type.toString() == PacketType.JOIN.toString()) {
       print("assigning id ${e.packet.id}");
       otherId = e.packet.id;
+    }
+  });
+
+  qClient.onBinaryEvent.listen((RtcEvent e) {
+    if (e is BinaryChunkEvent) {
+      BinaryChunkEvent bce = e;
+      output.nodes.clear();
+      output.appendHtml("chunk ${bce.sequence} of ${bce.totalSequences} ${bce.bytes} ${bce.bytesLeft}<br>");
+
+    } else if (e is BinarySendCompleteEvent) {
+      BinarySendCompleteEvent bsce = e;
+      output.nodes.clear();
+      output.appendHtml("complete sequence ${bsce.sequence}<br>");
+    } else if (e is BinaryBufferComplete) {
+      BinaryBufferComplete bbc = e;
+      output.appendHtml("Buffer complete ${bbc.buffer.byteLength}<br>");
     }
   });
 
