41c25798ec902bd9c8a59355285fe36a0d1bd4c7
Sami Ylonen <sami.ylonen@gmail.com>
2013-04-04 20:47:59 +0200

    fixes

--- web/canvasdraw_sample.dart
+++ web/canvasdraw_sample.dart
@@ -39,9 +39,9 @@ void main() {
 
   client.onDataChannelStateChangeEvent.listen((DataChannelStateChangedEvent e) {
     if (e.state == DATACHANNEL_OPEN) {
-      draw.addPeer(e.peerwrapper);
+      draw.addPeer(e.peerwrapper.id);
     } else if (e.state == DATACHANNEL_CLOSED) {
-      draw.removePeer(e.peerwrapper);
+      draw.removePeer(e.peerwrapper.id);
     }
   });
 
@@ -53,11 +53,11 @@ void main() {
       if (m.containsKey('packetType')) {
         int packetType = m['packetType'];
         if (packetType == PeerPacket.TYPE_START_DRAW) {
-          draw.startDraw(e.peer, StartDrawPacket.fromMap(m));
+          draw.startDraw(e.peer.id, StartDrawPacket.fromMap(m));
         } else if (packetType == PeerPacket.TYPE_UPDATE_DRAW) {
-          draw.updateDraw(e.peer, UpdateDrawPacket.fromMap(m));
+          draw.updateDraw(e.peer.id, UpdateDrawPacket.fromMap(m));
         } else if (packetType == PeerPacket.TYPE_END_DRAW) {
-          draw.endDraw(e.peer, EndDrawPacket.fromMap(m));
+          draw.endDraw(e.peer.id, EndDrawPacket.fromMap(m));
         }
       }
     }
@@ -69,7 +69,8 @@ void main() {
 class CanvasDraw {
   CanvasElement _element;
   CanvasRenderingContext2D _ctx;
-  List<DataPeerWrapper> _peerIds;
+  //List<DataPeerWrapper> _peerIds;
+  Map<String, Point> _peerIds;
   PeerPacket _toSend;
   ChannelClient _client;
   Timer _timer;
@@ -82,7 +83,7 @@ class CanvasDraw {
     _element.width = 770;
     _element.height = 400;
     _ctx = c.context2d;
-    _peerIds = new List<DataPeerWrapper>();
+    _peerIds = new Map<String, Point>();
 
     _client = client;
     _lastSent = new DateTime.now().millisecondsSinceEpoch;
@@ -90,38 +91,59 @@ class CanvasDraw {
     _element.onMouseDown.listen(_onMouseDown);
     _element.onMouseMove.listen(_onMouseMove);
     _element.onMouseUp.listen(_onMouseUp);
-    _element.onTouchStart(_onTouchStart);
+    _element.onTouchStart.listen(_onTouchStart);
   }
 
-  void addPeer(DataPeerWrapper wrapper) {
-    if (!_peerIds.contains(wrapper))
-      _peerIds.add(wrapper);
+  void addPeer(String id) {
+    window.setImmediate(() {
+      if (!_peerIds.containsKey(id))
+        _peerIds[id] = null;
+    });
   }
 
-  void removePeer(DataPeerWrapper wrapper) {
-    _peerIds.remove(wrapper);
+  void removePeer(String id) {
+    window.setImmediate(() {
+      if (!_peerIds.containsKey(id))
+        _peerIds.remove(id);
+    });
   }
 
-  void startDraw(DataPeerWrapper dpw, StartDrawPacket p) {
+  void startDraw(String id, StartDrawPacket p) {
 
     _ctx.beginPath();
     _ctx.moveTo(p.x, p.y);
-  }
-
-  void updateDraw(DataPeerWrapper dpw, UpdateDrawPacket p) {
-
     _ctx.lineTo(p.x, p.y);
     _ctx.stroke();
+    _peerIds[id] = new Point(p.x, p.y);
   }
 
-  void endDraw(DataPeerWrapper dpw, EndDrawPacket p) {
-
+  void updateDraw(String id, UpdateDrawPacket p) {
+    Point previous = _peerIds[id];
+    _ctx.beginPath();
+    _ctx.moveTo(previous.x, previous.y);
     _ctx.lineTo(p.x, p.y);
     _ctx.stroke();
+    _peerIds[id] = new Point(p.x, p.y);
+  }
+
+  void endDraw(String id, EndDrawPacket p) {
+
+    _ctx.beginPath();
+    _ctx.moveTo(p.x, p.y);
+    _ctx.lineTo(p.x, p.y);
+    _ctx.stroke();
+    _peerIds[id] = new Point(p.x, p.y);
   }
 
   void _onTouchStart(TouchEvent e) {
-
+    e.preventDefault();
+    
+    _isMouseDown = true;
+    int x = e.targetTouches[0].page.x;
+    int y = e.targetTouches[0].page.y;
+    _ctx.beginPath();
+    _ctx.moveTo(x, y);
+    _signalMouseDown(x, y);
   }
 
   void _onMouseDown(MouseEvent e) {
@@ -130,26 +152,67 @@ class CanvasDraw {
     _ctx.beginPath();
     _ctx.moveTo(e.offset.x, e.offset.y);
 
-    _toSend = new StartDrawPacket(e.offset.x, e.offset.y);
-    for (int i = 0; i < _peerIds.length; i++) {
-      PeerWrapper pw = _peerIds[i];
-      _client.sendArrayBufferReliable(pw.id, _toSend.toBuffer());
-    }
+    _signalMouseDown(e.offset.x, e.offset.y);
   }
-
+  
+  void _signalMouseDown(int x, int y) {
+    _toSend = new StartDrawPacket(x, y);
+    _peerIds.forEach((String id, Point p) {
+      _client.sendArrayBufferUnReliable(id, _toSend.toBuffer());
+    });
+    //for (int i = 0; i < _peerIds.length; i++) {
+    //  String id = _peerIds[i];
+    //  _client.sendArrayBufferUnReliable(pw.id, _toSend.toBuffer());
+    //}
+  }
+  
+  void _onTouchEnd(TouchEvent e) {
+    e.preventDefault();
+    
+    _isMouseDown = false;
+    int x = e.targetTouches[0].page.x;
+    int y = e.targetTouches[0].page.y;
+    _ctx.lineTo(x, y);
+    _ctx.stroke();
+    _signalMouseUp(x, y);
+  }
+  
   void _onMouseUp(MouseEvent e) {
     _isMouseDown = false;
 
     _ctx.lineTo(e.offset.x, e.offset.y);
     _ctx.stroke();
 
-    _toSend = new EndDrawPacket(e.offset.x, e.offset.y);
-    for (int i = 0; i < _peerIds.length; i++) {
-      PeerWrapper pw = _peerIds[i];
-      _client.sendArrayBufferReliable(pw.id, _toSend.toBuffer());
-    }
+    _signalMouseUp(e.offset.x, e.offset.y);
   }
 
+  void _signalMouseUp(int x, int y) {
+    _toSend = new EndDrawPacket(x, y);
+    _peerIds.forEach((String id, Point p) {
+      _client.sendArrayBufferUnReliable(id, _toSend.toBuffer());
+    });
+    //for (int i = 0; i < _peerIds.length; i++) {
+    //  PeerWrapper pw = _peerIds[i];
+    //  _client.sendArrayBufferUnReliable(pw.id, _toSend.toBuffer());
+    //}  
+  }
+  
+  void _onTouchMove(TouchEvent e) {
+    if (!_isMouseDown)
+      return;
+    
+    int x = e.targetTouches[0].page.x;
+    int y = e.targetTouches[0].page.y;
+    _ctx.lineTo(x, y);
+    _ctx.stroke(); 
+    
+    int now = new DateTime.now().millisecondsSinceEpoch;
+    if (now > _lastSent + _updateInterval) {
+      _signalMouseMove(x, y);
+      _lastSent = now;
+    }
+  }
+  
   void _onMouseMove(MouseEvent e) {
     if (!_isMouseDown)
       return;
@@ -159,15 +222,22 @@ class CanvasDraw {
 
     int now = new DateTime.now().millisecondsSinceEpoch;
     if (now > _lastSent + _updateInterval) {
-      _toSend = new UpdateDrawPacket(e.offset.x, e.offset.y);
-      for (int i = 0; i < _peerIds.length; i++) {
-        PeerWrapper pw = _peerIds[i];
-        _client.sendArrayBufferReliable(pw.id, _toSend.toBuffer());
-      }
+      _signalMouseMove(e.offset.x, e.offset.y);
       _lastSent = now;
     }
   }
 
+  void _signalMouseMove(int x, int y) {
+    _toSend = new UpdateDrawPacket(x, y);
+    _peerIds.forEach((String id, Point p) {
+      _client.sendArrayBufferReliable(id, _toSend.toBuffer());
+    });
+    //for (int i = 0; i < _peerIds.length; i++) {
+    //  PeerWrapper pw = _peerIds[i];
+    //  _client.sendArrayBufferReliable(pw.id, _toSend.toBuffer());
+    //}
+  }
+  
   void _onTick(Timer t) {
 
   }
